024f1c1a4d198ab14e03fd1017adacd1577102d1
jackwrfuller <jack.fuller101@gmail.com>
2025-10-17 15:19:51 +1100

    update config

--- playwright.config.ts
+++ playwright.config.ts
@@ -8,6 +8,9 @@ import { defineConfig, devices } from '@playwright/test';
 // import path from 'path';
 // dotenv.config({ path: path.resolve(__dirname, '.env') });
 
+const baseURL = process.env.BASE_URL || 'http://localhost:8888';
+
+
 /**
  * See https://playwright.dev/docs/test-configuration.
  */
@@ -26,7 +29,7 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('')`. */
-    // baseURL: 'http://localhost:3000',
+    baseURL,
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
